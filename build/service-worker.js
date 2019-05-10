0671ec4c8249645a3406cf96fc6553430f7989f7
Olga <o.viholainen@gmail.com>
2019-05-10 14:58:25 +0300

    did more tests with cypress and updated heroku app

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7270684a8be48392b6bbd525d611541b.js"
+  "/precache-manifest.40e4469119aff587d7d2c08f62905362.js"
 );
 
 workbox.clientsClaim();
